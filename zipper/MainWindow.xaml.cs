754bafe2f37f326470d5bc16bfcbb5733d7ac70d
forest611 <forest611328@gmail.com>
2018-12-16 18:55:08 +0900

    first commit

--- zipper/MainWindow.xaml.cs
+++ zipper/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,31 @@ namespace zipper
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public String path;
+        public String filename;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void zip(object sender, RoutedEventArgs e)
+        {
+            ZipFile.CreateFromDirectory(path,filename);
+        }
+
+        private void browse(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            
+            if (dialog.ShowDialog() == true)
+            {
+                path = dialog.FileName;
+                filename = dialog.SafeFileName;
+                pathname.Text = dialog.FileName;
+            }
         }
     }
 }
