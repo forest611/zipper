d164f800daab27b90c5c0f9a8870b9fd287f957f
forest611 <forest611328@gmail.com>
2019-01-08 21:42:57 +0900

    使用可能段階

--- zipper/MainWindow.xaml.cs
+++ zipper/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace zipper
 {
@@ -23,8 +14,9 @@ namespace zipper
     public partial class MainWindow : Window
     {
 
-        public String path;
-        public String filename;
+        public String place; //保存先
+        public String zipPath; //圧縮するフォルダのディレクトリ
+        public String fileName;
 
         public MainWindow()
         {
@@ -32,21 +24,60 @@ namespace zipper
         }
 
 
+        //圧縮処理
         private void zip(object sender, RoutedEventArgs e)
         {
-            ZipFile.CreateFromDirectory(path,filename);
+             
+            try
+            {
+                zipPath = pathname1.GetLineText(0);
+
+                place = pathname2.GetLineText(0) + @"\" + fileName;
+                if ((bool)timeCheck.IsChecked) place = place + " " + DateTime.Now.ToString("MMddHHmm");
+
+                ZipFile.CreateFromDirectory(zipPath,place+".zip", CompressionLevel.Optimal,false, Encoding.UTF8);
+                MessageBox.Show("ファイルの圧縮が完了しました");
+            }
+            catch (System.ArgumentNullException)
+            {
+                MessageBox.Show("圧縮するファイル、圧縮したファイルの保存先を設定してください");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("圧縮先のファイルにアクセスできませんでした");
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("圧縮済みの同名ファイルが存在する \n もしくは正しく記述できていない可能性があります");
+            }
+            place = "";
+
         }
 
-        private void browse(object sender, RoutedEventArgs e)
+        //圧縮するフォルダを選択
+        private void browse1(object sender, RoutedEventArgs e)
         {
-            var dialog = new OpenFileDialog();
-            
-            if (dialog.ShowDialog() == true)
-            {
-                path = dialog.FileName;
-                filename = dialog.SafeFileName;
-                pathname.Text = dialog.FileName;
-            }
+            zipPath = "";
+            var openDlg = new CommonOpenFileDialog();
+            openDlg.IsFolderPicker = true;
+            openDlg.ShowDialog();
+            pathname1.Text = openDlg.FileName;
+            fileName = System.IO.Path.GetFileName(openDlg.FileName);
+        }
+        //zipを保存する場所を設定
+        private void browse2(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog saveArea = new CommonOpenFileDialog();
+            saveArea.IsFolderPicker = true;
+            saveArea.ShowDialog();
+            pathname2.Text = saveArea.FileName;
+        }
+
+
+
+        private void preset(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
